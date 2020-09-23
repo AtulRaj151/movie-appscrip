ca2f1b007d75b2e3db86fa201fd07aa44e81f9ac
Atul Raj <atulsid151@gmail.com>
2020-09-23 18:25:20 +0530

    Updates

---
+++ precache-manifest.ff88135c2ad67f736e5d5090ebab2483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c37ed8e7a0dde021149f682309ce661",
+    "url": "/movie-appscrip/index.html"
+  },
+  {
+    "revision": "ae0976a9458bba471bd8",
+    "url": "/movie-appscrip/static/css/main.c3eac6f6.chunk.css"
+  },
+  {
+    "revision": "a701376213084706a478",
+    "url": "/movie-appscrip/static/js/2.b592f154.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-appscrip/static/js/2.b592f154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0976a9458bba471bd8",
+    "url": "/movie-appscrip/static/js/main.e2f5677f.chunk.js"
+  },
+  {
+    "revision": "3afbc1442dc197d17c0e",
+    "url": "/movie-appscrip/static/js/runtime-main.cdd01652.js"
+  }
+]);
